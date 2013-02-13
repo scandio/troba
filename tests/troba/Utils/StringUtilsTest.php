4ddb39f8c5574025dc16e2538a81ad85bae0fe01
Christian Koch <koch.chris@gmail.com>
2013-02-13 23:30:23 +0100

    namespace changes

--- tests/troba/Utils/StringUtilsTest.php
+++ tests/troba/Utils/StringUtilsTest.php
@@ -1,22 +1,22 @@
 <?php
 
-use troba\Util\StringUtils;
+use troba\Util\StringUtils as S;
 
 class StringUtilsTest extends PHPUnit_Framework_TestCase {
 
     function testCamelCase() {
-        $this->assertEquals('myString', StringUtils::camelCaseFrom('my-string'));
-        $this->assertEquals('myString', StringUtils::camelCaseFrom('my_string', '_'));
-        $this->assertEquals('myStringIsCool', StringUtils::camelCaseFrom('my#string#is#cool', '#'));
-        $this->assertEquals('myStringIsCool', StringUtils::camelCaseFrom('my_string_is_cool', '_'));
-        $this->assertEquals('my-stringIsCool', StringUtils::camelCaseFrom('my-string_is_cool', '_'));
+        $this->assertEquals('myString', S::camelCaseFrom('my-string'));
+        $this->assertEquals('myString', S::camelCaseFrom('my_string', '_'));
+        $this->assertEquals('myStringIsCool', S::camelCaseFrom('my#string#is#cool', '#'));
+        $this->assertEquals('myStringIsCool', S::camelCaseFrom('my_string_is_cool', '_'));
+        $this->assertEquals('my-stringIsCool', S::camelCaseFrom('my-string_is_cool', '_'));
 
-        $this->assertEquals('my-string', StringUtils::camelCaseTo('myString'));
-        $this->assertEquals('my-string', StringUtils::camelCaseTo('MyString'));
-        $this->assertEquals('my_string', StringUtils::camelCaseTo('myString', '_'));
-        $this->assertEquals('my-string-is-cool', StringUtils::camelCaseTo('myStringIsCool'));
-        $this->assertEquals('my_string_is_cool', StringUtils::camelCaseTo('myStringIsCool', '_'));
-        $this->assertEquals('my string is cool', StringUtils::camelCaseTo('myStringIsCool', ' '));
+        $this->assertEquals('my-string', S::camelCaseTo('myString'));
+        $this->assertEquals('my-string', S::camelCaseTo('MyString'));
+        $this->assertEquals('my_string', S::camelCaseTo('myString', '_'));
+        $this->assertEquals('my-string-is-cool', S::camelCaseTo('myStringIsCool'));
+        $this->assertEquals('my_string_is_cool', S::camelCaseTo('myStringIsCool', '_'));
+        $this->assertEquals('my string is cool', S::camelCaseTo('myStringIsCool', ' '));
     }
 
 }
